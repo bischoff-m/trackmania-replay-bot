51964db923b87dbfd48c0329d14fd257b68211f2
Marco Bischoff <bischoff_m@aim.com>
2023-08-08 21:50:40 +0200

    Add steps to control replay picker

--- packages/render/src/steps.py
+++ packages/render/src/steps.py
@@ -3,7 +3,13 @@ from dataclasses import dataclass
 from typing import Callable, List, Literal, Optional
 
 from api import Bob, LocateImageException
-from pynput.keyboard import Key
+
+
+class ControlFlowException(Exception):
+    def __str__(self):
+        return (
+            f"There was a general problem with controlling Trackmania: {self.args[0]}"
+        )
 
 
 @dataclass
@@ -89,7 +95,7 @@ class Controller:
             <ul>
                 <li>Trackmania is in "Windowed Borderless" mode</li>
                 <li>Trackmania is on the main monitor (restricted by pyautogui)</li>
-                <li>Optional: HDR disabled (not tested with HDR)</li>
+                <li>Optional: HDR disabled (works for me but could cause problems)</li>
             </ul>
             """
         )
@@ -109,7 +115,7 @@ class Controller:
                 ),
                 Button(
                     name="Step by Step",
-                    action=Controller.step_replaypicker_up,
+                    action=Controller.step_menu_create,
                     style="confirm",
                 ),
             ],
@@ -174,7 +180,7 @@ class Controller:
         Bob().clickImage(
             "Menu_ReplayEditorButton.png", retry="Menu_ReplayEditorButton_Hover.png"
         ).wait(0.2)
-        return Controller.state_end()
+        return Controller.step_replaypicker_up()
 
     @stepmethod(
         description="Trying to click the up button in the replay picker (if needed) to reach the root folder."
@@ -193,19 +199,57 @@ class Controller:
             found = Bob().findImage("ReplayPicker_EmptyPath.png") is not None
 
         if not found:
-            raise Exception("Could not direct the replay picker to the root folder.")
-        return Controller.state_end()
+            raise ControlFlowException(
+                "Could not direct the replay picker to the root folder."
+            )
+        return Controller.step_replaypicker_sort()
 
-    @stepmethod(description="Testing")
-    def step_testing():
-        test = {
-            "ListView": Bob().findImage("ReplayPicker_ListView.png"),
-            "TreeView": Bob().findImage("ReplayPicker_TreeView.png"),
-            "SortAscending": Bob().findImage("ReplayPicker_SortAscending.png"),
-            "SortDescending": Bob().findImage("ReplayPicker_SortDescending.png"),
-            "SortByTime": Bob().findImage("ReplayPicker_SortByTime.png"),
-            "SortByName": Bob().findImage("ReplayPicker_SortByName.png"),
-            "UpButton": Bob().findImage("ReplayPicker_UpButton.png"),
-        }
-        print(json.dumps(test, indent=4))
+    @stepmethod(description="Trying to activate tree view, sort by name and ascending.")
+    def step_replaypicker_sort():
+        # Activate tree view if not already active.
+        if Bob().findImage("ReplayPicker_ListView.png") is not None:
+            Bob().clickImage("ReplayPicker_ListView.png")
+        if Bob().findImage("ReplayPicker_TreeView.png") is None:
+            raise LocateImageException(
+                "ReplayPicker_ListView.png or ReplayPicker_TreeView.png"
+            )
+
+        # Sort by name if not already sorted by name.
+        if Bob().findImage("ReplayPicker_SortByTime.png") is not None:
+            Bob().clickImage("ReplayPicker_SortByTime.png")
+        if Bob().findImage("ReplayPicker_SortByName.png") is None:
+            raise LocateImageException(
+                "ReplayPicker_SortByTime.png or ReplayPicker_SortByName.png"
+            )
+
+        # Sort ascending if not already sorted ascending.
+        if Bob().findImage("ReplayPicker_SortDescending.png") is not None:
+            Bob().clickImage("ReplayPicker_SortDescending.png")
+        if Bob().findImage("ReplayPicker_SortAscending.png") is None:
+            raise LocateImageException(
+                "ReplayPicker_SortDescending.png or ReplayPicker_SortAscending.png"
+            )
+
+        return Controller.step_replaypicker_folder()
+
+    @stepmethod(
+        description="Trying to click the folder where this script saves the current replay and select it."
+    )
+    def step_replaypicker_folder():
+        Bob().clickText("!!! trackmania-replay-bot").wait(0.2)
+        Bob().clickText("ycnzzu02e5")
+        return Controller.step_replaypicker_confirm()
+
+    @stepmethod(
+        description='Trying to click the "CONFIRM" button and then the "EDIT" button.'
+    )
+    def step_replaypicker_confirm():
+        Bob().clickImage(
+            "ReplayPicker_ConfirmButton.png",
+            retry="ReplayPicker_ConfirmButton_Hover.png",
+        ).wait(0.2)
+        Bob().clickImage(
+            "ReplayPicker_EditButton.png",
+            retry="ReplayPicker_EditButton_Hover.png",
+        ).wait(0.2)
         return Controller.state_end()
